40569969a9bf5ae027eae7d1f793020a72178384
JunYoungKr <kop981020@naver.com>
2024-11-17 22:35:05 +0900

    feat: tsconfig.json 수정

--- alice_front/src/components/modal/modal.tsx
+++ alice_front/src/components/modal/modal.tsx
@@ -3,12 +3,9 @@ import Image, { StaticImageData } from "next/image";
 import hamburger from "../../app/images/hamburger.png";
 
 interface MenuItem {
-  id: number;
   name: string;
   englishName: string;
   price: string;
-  description: string;
-  image: StaticImageData;
 }
 interface ModalProps {
   item: MenuItem;
@@ -26,7 +23,6 @@ export default function Modal({ item, onClose }: ModalProps) {
         <h2>{item.name}</h2>
         <p>{item.englishName}</p>
         <p>{item.price}</p>
-        <p>{item.description}</p>
         <button className={styles.addButton}>장바구니에 추가</button>
       </div>
     </div>
